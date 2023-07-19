b1c332c6489ed4f0b3b3b64b82d3148779a07868
Helber <helbercillocomunica@gmail.com>
2023-07-18 20:33:00 -0500

    componente 5 y 6

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { C01componente } from './components/C01componente';
 import Contador1 from './components/C02componente';
 import Contador2 from './components/C03componente';
 import Contador3 from './components/C04componente';
+import C05operador from './components/C05operadorTemario';
+import C06matriz from './components/C06matriz1';
 
 function App() {
   return (
